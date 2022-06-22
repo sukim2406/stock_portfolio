7d28ee0fe72a5917806297be3121ccc070195dd3
Soun Sean Kim <sukim2406@gmail.com>
2022-06-22 21:29:11 +0900

    stock_portfolio : dev / deploy settings configured ::::: daily commit project 06-22-2022

---
+++ stock_portfolio_backend/stock_portfolio_backend/settings/deploy.py
@@ -0,0 +1,24 @@
+from .base import *
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+SECRET_KEY = env('SECRET_KEY')
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'password1234',
+        'HOST': 'mariadb',
+        'PORT': '3306',
+    }
+}
---
+++ stock_portfolio_backend/stock_portfolio_backend/settings/local.py
@@ -0,0 +1,20 @@
+from .base import *
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+SECRET_KEY = env('SECRET_KEY')
+
+DEBUG = env('DEBUG')
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
