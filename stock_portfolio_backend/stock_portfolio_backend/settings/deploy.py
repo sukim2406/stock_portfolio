3e796b768c6e97b8b6da9523e3c95f02f1fa2d70
Soun Sean Kim <sukim2406@gmail.com>
2022-06-23 11:59:39 +0900

    stock_portfolio: docker secret implemented ::::: daily commit project 06-23-2022

--- stock_portfolio_backend/stock_portfolio_backend/settings/deploy.py
+++ stock_portfolio_backend/stock_portfolio_backend/settings/deploy.py
@@ -1,12 +1,19 @@
 from .base import *
 
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
+
 env = environ.Env(
     DEBUG=(bool, False)
 )
 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 DEBUG = False
 
@@ -17,7 +24,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': 'password1234',
+        'PASSWORD': read_secret('MARIADB_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306',
     }
